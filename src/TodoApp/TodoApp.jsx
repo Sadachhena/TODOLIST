18d00407537aa94351a1dc51e9ae64bd85255dde
unknown <sadachhena@gmail.com>
2026-02-16 03:34:55 -0800

    Updated Tailwind config and project changes

--- src/TodoApp/TodoApp.jsx
+++ src/TodoApp/TodoApp.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function TodoApp() {
   return (
-    <div>
-      
-    </div>
+    <section>
+      <h1 className='font-bold text-4xl text-center'>Todo List</h1>
+    </section>
   )
 }
